ad46e0f8ffb4ce8f6c4d477e630715108d9b60e2
Shailesh_18_ <rushikeshkanfade123@gmail.com>
2025-06-27 15:37:56 +0530

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-require('dotenv').config();
-import express from 'express';
-import cors from 'cors';
-import templateRoutes from './routes/template';
-import chatRoutes from './routes/chat';
-import { config } from './config/environment';
+require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import templateRoutes from "./routes/template";
+import chatRoutes from "./routes/chat";
+import { config } from "./config/environment";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ message: "Zap Code Backend API is running" });
+});
+
 // Setup routes
-app.use('/template', templateRoutes);
-app.use('/chat', chatRoutes);
+app.use("/template", templateRoutes);
+app.use("/chat", chatRoutes);
 
 app.listen(config.port, () => {
   console.log(`Gemini server running on http://localhost:${config.port}`);
