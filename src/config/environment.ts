d5ee0bf17874fc793c98af35e105d2bf72e15258
rushikesh <153095920+Shailesh2302@users.noreply.github.com>
2025-09-26 00:26:23 +0530

    model change

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,6 +1,6 @@
 export const config = {
   port: process.env.PORT || 3000,
   geminiApiKey: process.env.GEMINI_API_KEY,
-  geminiModel: process.env.GEMINI_MODEL || "gemini-1.5-flash",
+  geminiModel: process.env.GEMINI_MODEL || "gemini-2.5-pro",
   nodeEnv: process.env.NODE_ENV || "Production",
 };
